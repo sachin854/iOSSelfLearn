5cedc197e50c0d6a2f8497f35c3ebba2fbc8bceb
sachinacharya <sachinacharya856@gmail.com>
2024-03-03 22:53:53 +0530

    Initial Commit

---
+++ iosQiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosQiki
+//
+//  Created by Sachin on 03/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosQiki/iosQikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosQikiApp.swift
+//  iosQiki
+//
+//  Created by Sachin on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iosQikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
